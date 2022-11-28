464dedf7ef0a60acfdf2d2a872ccfab0a1ed40f2
ol4juwon <olajuwonlawal2012@gmail.com>
2022-11-28 08:57:13 +0100

    updates

--- src/components/room/chessboard/EvenBoard.tsx
+++ src/components/room/chessboard/EvenBoard.tsx
@@ -21,8 +21,34 @@ const EvenBoard: FC<Props> = ({ chessPieces, index, isEven }: Props) => {
       <div className="w-1/5 flex flex-col justify-end px-1">
         {chessPieces[index].let}
       </div>
-      <div className="w-3/5">{chessPieces[index].icon}</div>
-      <div className="w-1/5">{chessPieces[index].num}</div>
+      <div className="w-3/5 h-full flex flex-col items-center justify-end py-2 ">
+        <i
+          className={`fas fa-chess-${
+            chessPieces[index].icon
+          } text-xl md:text-5xl 
+       ${
+         chessPieces[index].color == "white"
+           ? " text-colors-white "
+           : " text-colors-black"
+       }  `}
+        ></i>
+      </div>
+      <div
+        className={`w-1/5 text-xs md:text-md 
+      ${
+        isEven
+          ? (index + 1) % 2 == 0
+            ? "  text-text-bron"
+            : " text-off-bron "
+          : (index + 1) % 2 == 0
+          ? "  text-off-bron"
+          : " text-text-bron"
+      }
+      
+      `}
+      >
+        {chessPieces[index].num}
+      </div>
     </div>
   );
 };
